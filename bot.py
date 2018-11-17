4e0b2e5c8e66b88a52cf361aaa12ad236b7a3743
itsMad22 <45109068+itsMad22@users.noreply.github.com>
2018-11-17 00:00:46 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,6 +61,13 @@ async def on_message(message):
             await client.delete_message(message)
             await client.delete_message(tempc)
             
+    if message.content.upper().startswith('?PLAY'):
+        if message.channel !== 510207536926949376:
+        await client.delete_message(message)
+            tempc = await client.send_message(message.channel, "<@%s> Please use all dyno commands in #bot-spam, not here! Than" % (userID) )
+        time.sleep(3)
+        await client.delete_message(tempc)
+        
     if message.content.upper().startswith('!SC'):
             if "506131078017318913" in [role.id for role in message.author.roles]:
                 userID = message.author.id
