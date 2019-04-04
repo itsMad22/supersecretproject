8f540b39921501efa33a310ea08a1b0cceaad266
itsMad22 <45109068+itsMad22@users.noreply.github.com>
2019-04-04 20:49:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,26 @@
 import discord
 import time
 import asyncio
-import os
 
 from discord.ext.commands import Bot
 from discord.ext import commands
 from discord.utils import get
 
 
-bot_token = os.environ['BOT_TOKEN']
 
 Client = discord.Client() #Initialise Client 
 client = commands.Bot(command_prefix = "?") #Initialise client bot
 
-    
+
 @client.event 
 async def on_ready():    
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
     print("Noxu is now active.")
-    await client.change_presence(game=discord.Game(name='Protecting Servers'))
-
-@client.command(pass_context=True)
-async def clear(ctx, amount=10):
-    channel = ctx.message.channel
-    messages = []
-    async for message in client.logs_from(channel, limit=int(amount)):
-        messages.append(message)
-    await client.delete_messages(messages)
-    await client.send_message(message.channel, "Messages have been cleared.")
-
     
-@client.event
-async def on_member_join(member):
-    channel = member.server.get_channel("513337256950628382")
-    userID = member.id
-    await client.send_message(channel, "<@%s> has just joined the server!" % (userID) )
-   
 @client.event
 async def on_message(message):
     #if message.content == "cookie":
     #   await client.send_message(message.channel, ":cookie:")
-    
     if message.content.upper() == "BADWORD":
         await client.delete_message(message)
         userID = message.author.id
@@ -50,11 +30,7 @@ async def on_message(message):
     
     if message.content.upper().startswith('!PING'):
         userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!"+  `${Date.now() - message.createdTimestamp}` + ' ms`' % (userID) )
-        
-    if message.content.upper().startswith('!INFO'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Noxu bot is created by <@!124964742371475458> as a personal project, a utility and server moderation bot for the Noxu Gaming Community. Currently in alpha and not released for public servers." % (userID) )
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID) )
 
     if message.content.upper().startswith('!SAY'):
         if message.author.id == "124964742371475458":
@@ -68,13 +44,6 @@ async def on_message(message):
             await client.delete_message(message)
             await client.delete_message(tempc)
             
-#    if message.content.upper().startswith('?PLAY'):
-#        if message.channel != 510207536926949376:
-#            await client.delete_message(message)
-#            tempc = await client.send_message(message.channel, "<@%s> Please use all dyno commands in #bot-spam, not here! Than" % (userID) )
-#            time.sleep(3)
-#            await client.delete_message(tempc)
-        
     if message.content.upper().startswith('!SC'):
             if "506131078017318913" in [role.id for role in message.author.roles]:
                 userID = message.author.id
@@ -94,4 +63,4 @@ async def on_message(message):
             
 
 
-client.run(bot_token)
+client.run('NTEzMDI4MTI2MDAzOTUzNjc1.DtCErQ.c9Uw69qZdSswR3VyPR3USmWfnsk')
