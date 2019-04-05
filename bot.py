47250d4ea44123ad23f62fbb128eea2234a6b90b
itsMad22 <45109068+itsMad22@users.noreply.github.com>
2019-04-05 20:11:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,61 +6,26 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 from discord.utils import get
 
+#extensions = ['admin']
 
+#bot = discord.bot() #Initialise bot
+bot = commands.Bot(command_prefix = "$", status=discord.Status.idle, activity=discord.Game(name="Developed by Toast!")) #Initialise bot bot
 
-Client = discord.Client() #Initialise Client 
-client = commands.Bot(command_prefix = "?") #Initialise client bot
-
-
-@client.event 
-async def on_ready():    
+@bot.event
+async def on_ready():
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
     print("Noxu is now active.")
-    
-@client.event
-async def on_message(message):
-    #if message.content == "cookie":
-    #   await client.send_message(message.channel, ":cookie:")
-    if message.content.upper() == "BADWORD":
-        await client.delete_message(message)
-        userID = message.author.id
-        tempc = await client.send_message(message.channel, "<@%s> Please refrain from saying that here." % (userID) )
-        time.sleep(3)
-        await client.delete_message(tempc)
-    
-    if message.content.upper().startswith('!PING'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!" % (userID) )
+    #bot.load_extension("admin")
+    print("Loaded Admin Module")
 
-    if message.content.upper().startswith('!SAY'):
-        if message.author.id == "124964742371475458":
-            args = message.content.split(" ")
-            await client.delete_message(message)
-            await client.send_message(message.channel, "%s" % (" ".join(args[1:]))) # Say after array pos 1
-        else:
-            userID = message.author.id
-            tempc = await client.send_message(message.channel, "<@%s> You have insufficient permissions to run that command." % (userID) )
-            time.sleep(3)
-            await client.delete_message(message)
-            await client.delete_message(tempc)
-            
-    if message.content.upper().startswith('!SC'):
-            if "506131078017318913" in [role.id for role in message.author.roles]:
-                userID = message.author.id
-                tempc = await client.send_message(message.channel, "<@%s> Staff check returned true." % (userID) )
-                time.sleep(3)
-                await client.delete_message(message)
-                await client.delete_message(tempc)
-            else:
-                userID = message.author.id
-                tempc = await client.send_message(message.channel, "<@%s> You have insufficient permissions to run that command." % (userID) )
-                time.sleep(3)
-                await client.delete_message(message)
-                await client.delete_message(tempc)
-#
-#     <@&391655322885816332> - Staff Role ID
-#     <@&506131078017318913> - Management Role ID
-            
+@bot.command()
+async def info(ctx):
+    await ctx.send("Noxu bot is being developed by <@124964742371475458>, and is still in alpha stages.")
 
+@bot.command()
+async def ping(ctx):
+    ping_ = bot.latency
+    ping = round(ping_ * 1000)
+    await ctx.channel.send(f"My ping is {ping}ms")
 
-client.run('NTEzMDI4MTI2MDAzOTUzNjc1.DtCErQ.c9Uw69qZdSswR3VyPR3USmWfnsk')
+bot.run('NTEzMDI4MTI2MDAzOTUzNjc1.XKdDxQ.c  JO-X1AsmFW05h8TpPl3-wZFtZY')
