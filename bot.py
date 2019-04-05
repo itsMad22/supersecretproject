e8c35750f384834686518f7595154c78079c032b
itsMad22 <45109068+itsMad22@users.noreply.github.com>
2019-04-05 23:45:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,12 +20,28 @@ async def on_ready():
 
 @bot.command()
 async def info(ctx):
-    await ctx.send("Noxu bot is being developed by <@124964742371475458>, and is still in alpha stages.")
+    #await ctx.send("Noxu bot is being developed by <@124964742371475458>, and is still in alpha stages.")
+    embed = discord.Embed(
+        title = "Noxu Bot", description = "Noxu bot is still in development, and not released for public servers. Coded by <@124964742371475458>.", colour = discord.Colour.blue())
+    message = ctx.message ## Reference to message
+    await ctx.channel.send(embed=embed) # Send the message
+    await message.delete() ## Delete The message
 
 @bot.command()
 async def ping(ctx):
     ping_ = bot.latency
     ping = round(ping_ * 1000)
-    await ctx.channel.send(f"My ping is {ping}ms")
+    await ctx.channel.send("My ping is {ping}ms")
+
+@bot.command()
+async def say(ctx, *, message):
+    await ctx.send(message)
+    await ctx.message.delete()
+
+@bot.command()
+async def mentionme(ctx):
+    await ctx.send('{0.mention}, Testing!'.format(ctx.author))
+    await ctx.message.delete()
+
 
 bot.run('NTEzMDI4MTI2MDAzOTUzNjc1.XKdDxQ.c  JO-X1AsmFW05h8TpPl3-wZFtZY')
